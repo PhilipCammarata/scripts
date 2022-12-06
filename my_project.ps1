0929b6ad1742dceee9555f1583d916f05e8e1f4d
Philip Cammarata <philip@cammarata.me>
2022-12-06 14:00:04 -0500

    Consolidated my scripts into a module.

---
+++ my_project.ps1
@@ -0,0 +1,3 @@
+# Scenario: set up three windows virtual machines that are all members of the MSSA domain and get them ready to serve.
+
+Import-Module C:\Users\pcammarata\Projects\scripts\New-MSSAVM.ps1
