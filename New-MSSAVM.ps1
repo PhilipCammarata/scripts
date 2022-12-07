d733374726e2d2b0cc2f562455549d556e2c4c88
Philip Cammarata <philip@cammarata.me>
2022-12-07 13:34:55 -0500

    Prepping script for presentation.

--- New-MSSAVM.ps1
+++ New-MSSAVM.ps1
@@ -1,5 +1,5 @@
 function New-MSSAVM {
-    #[CmdletBinding()]
+    [CmdletBinding()]
     param (
         [Parameter(Mandatory, ValueFromPipeline)]
         [string]$VMName,
@@ -22,17 +22,21 @@ function New-MSSAVM {
             Write-Error "Virtual Machine with name $VMName already exists."
             Exit
         } else {
-            New-VM -Name $VMName -Path "D:\Hyper-V" -NewVHDPath "$VMName.vhdx" -NewVHDSizeBytes $StorageSize -MemoryStartupBytes $MemorySize -Generation 2
+            Write-Host "Creating new Virtual Machine called $VMName with $([math]::round($MemorySize/1GB, 2))GB memory and a $([math]::round($StorageSize/1GB, 2))GB VHDX." -ForegroundColor Green
+            New-VM -Name $VMName -Path "D:\Hyper-V" -NewVHDPath "$VMName.vhdx" -NewVHDSizeBytes $StorageSize -MemoryStartupBytes $MemorySize -Generation 2 -ErrorAction Stop
         }
 
         # Get and connecte the External Virtual Switch to the VM
         $VMSwitch = Get-VMSwitch -Name "External Virtual Switch"
+        Write-Host "Attaching $($VMSwitch.Name) to $VMName." -ForegroundColor Green
         Connect-VMNetworkAdapter -VMName $VM.Name -Name "Network Adapter" -SwitchName $VMSwitch.Name
 
         # Set the processor count to 4 as the lab environment has 8 cores overall
+        Write-Host "Setting the processor count to 4 for $VMName" -ForegroundColor Green
         Set-VMProcessor -VM $VM -Count 4
 
         # Enabled dynamic memory as much of the time the server will not need the full amount
+        Write-Host "Enabling Dynamic Memory for $VMName" -ForegroundColor Green
         Set-VMMemory -VM $VM -DynamicMemoryEnabled $true
 
         # Attach boot ISO
@@ -40,3 +44,7 @@ function New-MSSAVM {
         Set-VMFirmware -VM $VM -FirstBootDevice (Get-VMDvdDrive -VM $VM)
     }
 }
+
+New-MSSAVM -VMName "WS2022-MEMBER"
+
+# Invoke-Command -FilePath .\New-MSSAVM.ps1 -ComputerName OCTOPUS
